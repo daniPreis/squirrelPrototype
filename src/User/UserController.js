318493a7ed89a9da34fc887b3334777d125b60d1
Dani <daniela.preis002@stud.fh-dortmund.de>
2018-11-04 22:38:53 +0100

    Prototyp for Squirrelproject

---
+++ src/User/UserController.js
@@ -0,0 +1,89 @@
+import React from "react";
+import UserView from "./UserView";
+import axios from 'axios';
+
+class UserController extends React.Component {
+    state = {
+        pokemonImage: '1.gif',
+        pokemonName: '',
+
+        name: '',
+        surname: '',
+        height: '',
+        weight: '',
+
+        finalValue:'',
+    }
+
+    setName = (e) => {
+        this.setState({name: e.target.value})
+    }
+
+    setSurname = (e) => {
+        this.setState({surname: e.target.value})
+    }
+
+    setWeight = (e) => {
+        this.setState({weight: e.target.value})
+    }
+
+    setHeight = (e) => {
+        this.setState({height: e.target.value})
+    }
+
+
+    clearAll = () =>{
+        this.setState({ name: '' })
+        this.setState({ surname: '' })
+        this.setState({ height: '' })
+        this.setState({ weight: '' })
+    }
+
+    postData = ()=>{
+        fetch('http://localhost:8080/person', {
+            method: 'post',
+            headers: {'Content-Type':'application/json'},
+            body: {
+                "name": this.state.name,
+                "surname": this.state.surname,
+                "height": parseInt( this.state.height),
+                "wheight": parseFloat(this.state.weight),
+            }
+        });
+    }
+
+    getData =()=>{
+        axios.get('http://localhost:8080/person?surname=Mustermann&name=Kerstin&weight=60&height=160')
+            .then(response => this.setState({finalValue: response.data.name}))
+    }
+
+    addUser = () => {
+        this.postData();
+        this.getData();
+        alert("Benutzer hinzugefügt" + this.state.finalValue);
+        this.clearAll();
+    }
+
+    render() {
+
+        return (
+            <UserView
+                setName={this.setName}
+                setSurname={this.setSurname}
+                setWeight={this.setWeight}
+                setHeight={this.setHeight}
+
+
+                name={this.state.name}
+                surname={this.state.surname}
+                height={this.state.height}
+                weight={this.state.weight}
+
+                addUser={this.addUser}
+                shouldDisableSubmit={!this.state.name}
+    />
+    )
+    }
+}
+
+export default UserController
